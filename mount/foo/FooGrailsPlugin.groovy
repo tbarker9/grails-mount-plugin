6fa0f49e321b9ad612f6afdaf4db5b5cdbc613ad
tbarker <mingus.karate@gmail.com>
2013-12-01 14:16:30 -0500

    add wrapper and sample mount

---
+++ mount/foo/FooGrailsPlugin.groovy
@@ -0,0 +1,11 @@
+class FooGrailsPlugin {
+    def version = "0.1"
+    def grailsVersion = "2.2 > *"
+    def title = "Foo Plugin" // Headline display name of the plugin
+    def author = "Tommy Barker"
+    def authorEmail = "mingus.karate@gmail.com"
+    def description = '''\
+A mountable plugin to be used as an example
+'''
+    def documentation = "http://grails.org/plugin/foo"
+}
